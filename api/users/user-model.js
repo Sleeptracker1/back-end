2519b8ce0bd7ec1d6d8194ef1d67a4bf84f802c6
Matthew Bolton <matthewwbolton@gmail.com>
2020-04-28 09:31:39 -0400

    added JWT_SECRET to .env for authentication middleware

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -1,35 +1,28 @@
-const db = require('../../data/dbconfig')
+const db = require("../../data/dbconfig");
 
 module.exports = {
   getByID,
   addUser,
   findBy,
-  getUsers
+  getUsers,
+};
+
+function getByID(id) {
+  return db("users").where({ id }).first();
 }
 
-
-function getByID(id){
- return db('users')
-          .where({ id })
-          .first()
+function addUser(user) {
+  return db("users")
+    .insert(user)
+    .then(([id]) => {
+      return getByID(id);
+    });
 }
 
-
-function addUser(user){
-  return db('users')
-          .insert(user)
-          .then(([id]) => {
-            return getByID(id)
-          })
+function findBy(filter, val) {
+  return db("users").where(filter, val).first();
 }
 
-
-function findBy(filter, val){
-  return db('users')
-            .where(filter, val)
-            .first()
-}
-
-function getUsers(){
-  return db('users')
+function getUsers() {
+  return db("users");
 }
